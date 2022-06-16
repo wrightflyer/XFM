0f50d268f6f14f788b16df12254dc4d4875a6de1
Cliff Lawson <not@real.email>
2022-06-16 15:38:03 +0100

    Refactor to prevent duplicated code
    (and replace the readme picture with slightly
    modified layout)

--- adsr.py
+++ adsr.py
@@ -4,7 +4,7 @@ window = tk.Tk()
 window.title("Envelope")
 window.geometry("560x480")
 
-def alChange(val):
+def update(val):
     plot.delete("all")
     ax = atVar.get()
     ay = 256 - (2 * alVar.get())
@@ -26,48 +26,32 @@ def alChange(val):
     plot.create_line(sx, sy, sx + padding, sy, width=3, dash=(3,1))
     plot.create_line(sx + padding, sy, 512, ry, width=3)
 
+def sliders(label, xpos, varLevel, varTime):
+    ypos=290
+    lbl = tk.Label(text=label, font=("Arial", 24))
+    lbl.place(x=xpos + 5, y=ypos)
+    scale1 = tk.Scale(window, label="level", variable=varLevel, from_=127, to=0, command=update)
+    scale1.place(x=xpos + 30, y=ypos)
+    scale2 = tk.Scale(window, orient=tk.HORIZONTAL, label="time", variable=varTime, from_=0, to=127, command=update)
+    scale2.place(x=xpos, y=ypos + 110)
+
 plot = tk.Canvas(window, bg="#E0E0E0", width = 512, height = 256)
 plot.place(x=10, y=10)
 
-albl = tk.Label(text="A", font=("Arial", 24))
-albl.place(x=15, y=290)
 alVar = tk.IntVar()
-alevel = tk.Scale(window, label="level", variable = alVar, from_=127, to=0, command=alChange)
-alevel.place(x=40, y=290)
-
 atVar = tk.IntVar()
-atime = tk.Scale(window, orient=tk.HORIZONTAL, label="time", variable = atVar, from_=0, to=127, command=alChange)
-atime.place(x=10, y=400)
+sliders("A", 10, alVar, atVar)
 
-dlbl = tk.Label(text="D", font=("Arial", 24))
-dlbl.place(x=135, y=290)
 dlVar = tk.IntVar()
-dlevel = tk.Scale(window, label="level", variable = dlVar, from_=127, to=0, command=alChange)
-dlevel.place(x=160, y=290)
-
 dtVar = tk.IntVar()
-dtime = tk.Scale(window, orient=tk.HORIZONTAL, label="time", variable = dtVar, from_=0, to=127, command=alChange)
-dtime.place(x=130, y=400)
+sliders("D", 140, dlVar, dtVar)
 
-slbl = tk.Label(text="S", font=("Arial", 24))
-slbl.place(x=255, y=290)
 slVar = tk.IntVar()
-slevel = tk.Scale(window, label="level", variable = slVar, from_=127, to=0, command=alChange)
-slevel.place(x=280, y=290)
-
 stVar = tk.IntVar()
-stime = tk.Scale(window, orient=tk.HORIZONTAL, label="time", variable = stVar, from_=0, to=127, command=alChange)
-stime.place(x=250, y=400)
+sliders("S", 270, slVar, stVar)
 
-rlbl = tk.Label(text="R", font=("Arial", 24))
-rlbl.place(x=365, y=290)
 rlVar = tk.IntVar()
-rlevel = tk.Scale(window, label="level", variable = rlVar, from_=127, to=0, command=alChange)
-rlevel.place(x=400, y=290)
-
 rtVar = tk.IntVar()
-rtime = tk.Scale(window, orient=tk.HORIZONTAL, label="time", variable = rtVar, from_=0, to=127, command=alChange)
-rtime.place(x=370, y=400)
-
+sliders("R", 400, rlVar, rtVar)
 
 window.mainloop()
