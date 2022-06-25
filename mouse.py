9a682190cc1d5c9ddbc26e146a585e49a04d5d76
Cliff Lawson <wrightflyer@hotmail.com>
2022-06-25 23:25:01 +0100

    Experiment with on/off button

--- mouse.py
+++ mouse.py
@@ -3,10 +3,12 @@ from tkinter import *
 from PIL import Image, ImageTk
 
 index = 0
+index2 = 0
 prevy = 0
 
 def motion(event):
     global index
+    global index2
     global prevy
     newFrame = False
     print(f"x={event.x}, y={event.y}")
@@ -18,6 +20,13 @@ def motion(event):
         if index > 0:
             index = index -1
             newFrame = True
+    if event.y != prevy:
+        index2 = index2 + 1
+        if index2 > 1:
+            index2 = 0
+        print("update pic2", index)
+        canvas.delete("pic2")
+        canvas.create_image(130, 10, anchor=tk.NW, image = frames2[index2], tag="pic2")
     prevy = event.y
     if newFrame == True:
         canvas.delete("pic")
@@ -39,9 +48,23 @@ for n in range(numFrames):
     frame = ImageTk.PhotoImage(img.crop(tup))
     frames.append(frame)
 
-canvas = Canvas(window, width = 140, height = 140, bg='#313132')
+img2 = Image.open("on_off.png")
+width2 = img2.size[0]
+height2 = img2.size[1]
+#vertically stitched PNG
+numFrames2 =2
+
+frames2 = []
+for n in range(numFrames2):
+    tup = (0, 41 * n, width2, 41 * (n + 1))
+    frame = ImageTk.PhotoImage(img2.crop(tup))
+    frames2.append(frame)
+print(frames2)
+
+canvas = Canvas(window, width = 220, height = 140, bg='#313132')
 canvas.place(x=40, y=40)
 
 canvas.create_image(10, 10, anchor=tk.NW, image = frames[index], tag="pic")
+canvas.create_image(130, 10, anchor=tk.NW, image = frames2[index2], tag="pic2")
 
 window.mainloop()
