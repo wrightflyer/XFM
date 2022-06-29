ac27a9cb41b0a98a4233d1b867ae194ca53252e4
Cliff Lawson <not@real.email>
2022-06-29 15:17:48 +0100

    make mouse/anims more 'classy' and fix panel a bit

--- mouse.py
+++ mouse.py
@@ -7,9 +7,12 @@ index2 = 0
 prevy = 0
 
 class Anim:
-    def __init__(self, fname, numFrames):
+    def __init__(self, fname, numFrames, xpos, ypos):
+        self.fname = fname
         self.frames = []
         self.index = 0
+        self.xpos = xpos
+        self.ypos = ypos
         self.img = Image.open(fname)
         self.width = self.img.size[0]
         self.height = self.img.size[1]
@@ -22,7 +25,7 @@ class Anim:
         print(self.width, self.height, self.frameHeight, self.numFrames)
 
     def getFrame(self):
-        print("returning frame ", self.index)
+        print(self.fname, "returning frame ", self.index)
         return self.frames[self.index]
 
     def setIndex(self, n):
@@ -32,80 +35,64 @@ class Anim:
         returrn (self.width, self.height, self.frameHeight)
 
     def inc(self):
-        if (self.index < self.numFrames):
+        #print("consider inc, index=", self.index, "limit = ", self.numFrames)
+        if (self.index < (self.numFrames - 1)):
             self.index = self.index + 1
-            print("inc to", self.index)
+            print(self.fname, "inc to", self.index)
 
     def dec(self):
         if self.index >= 1:
             self.index = self.index - 1
-            print("dec to", self.index)
+            print(self.fname, "dec to", self.index)
+
+    def draw(self, cv):
+        cv.delete(self.fname)
+        cv.create_image(self.xpos, self.ypos, anchor=tk.NW, image = self.getFrame(), tag=self.fname)
+
+    def click(self, cv, x, y):
+        if x >= self.xpos and x <= (self.xpos + self.width):
+            if y >= self.ypos and y <= (self.ypos + self.frameHeight):
+                self.index = self.index + 1
+                if self.index >= self.numFrames:
+                    self.index = 0
+                self.draw(cv)
 
 def motion(event):
-#    global index
-#    global index2
-#    global prevy
-#    newFrame = False
-#    print(f"x={event.x}, y={event.y}")
-    if event.y > prevy:
-        anim1.inc()
-        anim2.inc()
-#        if index < (numFrames - 1):
-#            index = index + 1
-#            newFrame = True
+    global prevy
+    newFrame = False
+
     if event.y < prevy:
+        anim1.inc()
+        #anim2.inc()
+        newFrame = True
+
+    if event.y > prevy:
         anim1.dec()
-        anim2.dec()
-#        if index > 0:
-#            index = index -1
-#            newFrame = True
-#    if event.y != prevy:
-#        index2 = index2 + 1
-#        if index2 > 1:
-#            index2 = 0
-#        print("update pic2", index)
-        canvas.delete("pic2")
-        canvas.create_image(130, 10, anchor=tk.NW, image = anim2.getFrame(), tag="pic2")
-#    prevy = event.y
-#    if newFrame == True:
-        canvas.delete("pic")
-        canvas.create_image(10, 10, anchor=tk.NW, image = anim1.getFrame(), tag="pic")
+        #anim2.dec()
+        newFrame = True
+
+    if newFrame == True:
+        anim1.draw(canvas)
+        #anim2.draw(canvas)
+
+    prevy = event.y
+
+def button(event):
+    anim2.click(canvas, event.x, event.y)
+    #print(event)
 
 window = Tk()
 window.geometry("640x480")
 window.bind('<B1-Motion>', motion)
+window.bind('<Button>', button)
 
-anim1 = Anim("possible2.png", 128)
-#img = Image.open("possible2.png")
-#width = img.size[0]
-#height = img.size[1]
-#vertically stitched PNG
-#numFrames = int(height / width)
+anim1 = Anim("possible2.png", 128, 10, 10)
+anim2 = Anim("on_off2.png", 2, 130, 10)
 
-#frames = []
-#for n in range(numFrames):
-#    tup = (0, width * n, width, width * (n + 1))
-#    frame = ImageTk.PhotoImage(img.crop(tup))
-#    frames.append(frame)
-
-anim2 = Anim("on_off.png", 2)
-#img2 = Image.open("on_off.png")
-#width2 = img2.size[0]
-#height2 = img2.size[1]
-#vertically stitched PNG
-#numFrames2 =2
-
-#frames2 = []
-#for n in range(numFrames2):
-#    tup = (0, 41 * n, width2, 41 * (n + 1))
-#    frame = ImageTk.PhotoImage(img2.crop(tup))
-#    frames2.append(frame)
-#print(frames2)
-
-canvas = Canvas(window, width = 220, height = 140, bg='#313132')
+canvas = Canvas(window, width = 300, height = 140, bg='#313132')
 canvas.place(x=40, y=40)
 
-canvas.create_image(10, 10, anchor=tk.NW, image = anim1.getFrame(), tag="pic")
-canvas.create_image(130, 10, anchor=tk.NW, image = anim2.getFrame(), tag="pic2")
+anim1.draw(canvas)
+anim2.draw(canvas)
 
 window.mainloop()
