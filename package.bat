8c36c6f9c4c91c958b4a2fb8a5664cb094e24d25
Cliff Lawson <wrightflyer@hotmail.com>
2023-02-17 18:54:10 +0000

    Fix for https://github.com/wrightflyer/XFM/issues/3 (xbm files in package)

--- package.bat
+++ package.bat
@@ -3,4 +3,5 @@ pyinstaller -F xfm.py
 copy settings.json dist
 copy initpatch.json dist
 xcopy images_animations\* dist\images_animations\ /cher
+xcopy stipple\* dist\stipple\ /cher
 copy README.mhtml dist
