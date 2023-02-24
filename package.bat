a0e445571ae904fde9a6d45c6c5ee49ee0f1f68b
Cliff Lawson <wrightflyer@users.noreply.github.com>
2023-02-24 13:14:26 +0000

    Small typo in package.bat - fix before 1.0-release

--- package.bat
+++ package.bat
@@ -5,4 +5,4 @@ copy initpatch.json dist
 xcopy images_animations\* dist\images_animations\ /cher
 xcopy stipple\* dist\stipple\ /cher
 copy README.mhtml dist
-copy factory\* dist\factory\ /cher
+xcopy factory\* dist\factory\ /cher
